e8e5788d0b3bd3f9c9666babb6cc01f0dd34623c
Nurasylpro <nuramylt@gmail.com>
2024-11-16 11:36:48 +0600

    fixed external services cards

--- cadmium-app/src/components/ServiceProfile/index.jsx
+++ cadmium-app/src/components/ServiceProfile/index.jsx
@@ -5,12 +5,15 @@ export default function ServiceProfile({
   serviceName = "Green Wheels Service",
   serviceLocation = "Downtown City Center",
   serviceRating = "Rating: ★★★★☆",
+  url = "https://borze.kz/?utm_source=google&utm_medium=cpc&utm_campaign=g_search_kz_sto&utm_content=714646730524&utm_term=%D0%B0%D0%B2%D1%82%D0%BE%D1%81%D0%B5%D1%80%D0%B2%D0%B8%D1%81&gad_source=1&gclid=Cj0KCQiA_9u5BhCUARIsABbMSPt9WCjaeyHjbcxTKgNTjevZnDrvdQSyVzRvSIQBj9TIrB6Jx3NanjoaAqv4EALw_wcB", // Add the URL you want to navigate to
   ...props
 }) {
   return (
-    <div
-      {...props}
-      className={`${props.className} flex flex-col items-start w-[32%] md:w-full gap-[26px] px-[34px] py-10 sm:p-5 border-gray-900_01 border border-solid bg-white-a700 shadow-xs rounded-[44px]`}
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`${props.className} flex flex-col items-start w-[32%] md:w-full gap-[26px] px-[34px] py-10 sm:p-5 border-gray-900_01 border border-solid bg-white-a700 shadow-xs rounded-[44px] cursor-pointer`}
     >
       <div className="flex flex-col gap-7 self-stretch">
         <div className="flex items-center gap-5">
@@ -29,6 +32,6 @@ export default function ServiceProfile({
       <Text size="textxs" as="p" className="mb-1.5 text-[18px] font-normal text-black-900">
         {serviceRating}
       </Text>
-    </div>
+    </a>
   );
 }
